bfa358a897fc395f02ecb387f9258756d5f8ce39
Denis Abdrakhmanov <denisa37@mail.ru>
2023-03-16 12:51:38 +0300

    quest 4

--- src/Cli.php
+++ src/Cli.php
@@ -1,15 +1,19 @@
 <?php
- 
 namespace BrainGames\Cli;
  
 require_once __DIR__ . '/../vendor/autoload.php';
  
 use function cli\line;
 use function cli\prompt;
- 
+
+/**
+ * Greeting
+ *
+ * @param bool ()
+ */
 function greeting()
 {
-	    line('Welcome to the Brain Game!');
-	        $name = prompt('May I have your name?');
-	        line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+            $name = prompt('May I have your name?');
+            line("Hello, %s!", $name);
 }
